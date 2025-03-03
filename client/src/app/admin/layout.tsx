7c702cbf8d2a7f4ec91b26f047b0e7e85bd9975a
vanh <vietanhbg1508@gmail.com>
2025-03-03 18:02:53 +0700

    feat: add page layout

---
+++ client/src/app/admin/layout.tsx
@@ -0,0 +1,95 @@
+import { CircleUser, Menu, Store } from "lucide-react";
+
+import { Button } from "@/components/ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
+
+export default function AdminPage({ children }: { children: React.ReactNode }) {
+  return (
+    <div className="grid min-h-screen w-full md:grid-cols-[220px_1fr] lg:grid-cols-[280px_1fr]">
+      {/* Sidebar */}
+      <div className="hidden border-r bg-muted/40 md:block">
+        <div className="flex h-full max-h-screen flex-col gap-2">
+          <div className="flex h-14 items-center border-b px-4 lg:h-[60px] lg:px-6">
+            <a href="/" className="flex items-center gap-2 font-semibold">
+              <img src="/logo.png" className="h-6 w-6" />
+              <span className="">Trang quản lý</span>
+            </a>
+          </div>
+          <div className="flex-1">
+            <nav className="grid items-start px-2 text-sm font-medium lg:px-4">
+              <a
+                href="#"
+                className="flex items-center gap-3 rounded-lg bg-muted px-3 py-2 text-primary transition-all hover:text-primary"
+              >
+                <Store className="h-4 w-4" />
+                Danh sách cửa hàng{" "}
+              </a>
+            </nav>
+          </div>
+        </div>
+      </div>
+
+      <div className="flex flex-col">
+        <header className="flex h-14 items-center gap-4 border-b bg-muted/40 px-4 lg:h-[60px] lg:px-6">
+          {/* Sidebar responsive */}
+          <Sheet>
+            <SheetTrigger asChild>
+              <Button
+                variant="outline"
+                size="icon"
+                className="shrink-0 md:hidden"
+              >
+                <Menu className="h-5 w-5" />
+                <span className="sr-only">Toggle navigation menu</span>
+              </Button>
+            </SheetTrigger>
+            <SheetContent side="left" className="flex flex-col">
+              <nav className="grid gap-2 text-lg font-medium">
+                <a
+                  href="#"
+                  className="flex items-center gap-2 text-lg font-semibold"
+                >
+                  <img src="/logo.png" className="h-6 w-6" />
+                  <span className="">Trang quản lý</span>
+                </a>
+                <a
+                  href="#"
+                  className="mx-[-0.65rem] flex items-center gap-4 rounded-xl bg-muted px-3 py-2 text-foreground hover:text-foreground"
+                >
+                  <Store className="h-4 w-4" />
+                  Danh sách cửa hàng{" "}
+                </a>
+              </nav>
+            </SheetContent>
+          </Sheet>
+          {/* Avatar button */}
+          <DropdownMenu>
+            <DropdownMenuTrigger asChild>
+              <Button variant="secondary" size="icon" className="rounded-full">
+                <CircleUser className="h-5 w-5" />
+                <span className="sr-only">Toggle user menu</span>
+              </Button>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent align="end">
+              <DropdownMenuLabel>Sample name</DropdownMenuLabel>
+              <DropdownMenuSeparator />
+              <DropdownMenuItem>Hỗ trợ</DropdownMenuItem>
+              <DropdownMenuItem onClick={() => {}}>Đăng xuất</DropdownMenuItem>
+            </DropdownMenuContent>
+          </DropdownMenu>
+        </header>
+        <main className="grid flex-1 items-start gap-4 p-4 sm:px-6 sm:py-0 sm:pt-4 md:gap-8">
+          {children}
+        </main>
+      </div>
+    </div>
+  );
+}
